4db73eba9dfe50683efc55d57ebbe8a56c93e846
Bitspire1 <Bitspireone@proton.me>
2026-02-21 11:27:55 +0100

    optymalizacja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,17 +71,6 @@ export const metadata: Metadata = {
     icon: [
       { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/logo/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/logo/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/icons/apple-icon-180.png', sizes: '180x180', type: 'image/png' },
-    ],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/logo/safari-pinned-tab.svg',
-      },
     ],
   },
   category: 'technology',
